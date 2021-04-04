09c48df7f3c1b16d41c9f814eea261d4b379c6bb
alkarmaev <alkarmaev@mail.ru>
2021-04-04 17:53:21 +0300

    Мишке вопрос! почему не работает код?

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+s = 'Abracadabra'
+d = 0
+f = int(3)                  # вопрос!
+for i in int(f):
+    print(s[0:3])
