64d17f3157399aab8fc1a6703df4e34d5de73a69
alkarmaev <alkarmaev@mail.ru>
2021-05-04 22:02:56 +0300

    #Вывести все символы строки через один в обратном порядке, начиная с последнего. Проверяй!

---
+++ Task 1.py
@@ -0,0 +1,20 @@
+d = dict(short='dict', long='dictionary')
+print(d)
+
+d = dict([(1,1),(2,4)])
+print(d)
+
+d = {}
+print(d)
+
+d = {'dict':1,'dictionary':2}
+print(d)
+
+d = dict.fromkeys(['a','b'])
+print(d)
+
+d = dict.fromkeys(['a','b'],100)
+print(d)
+
+d= {a: a** 2 for a in range(99999999)}
+print(d)
